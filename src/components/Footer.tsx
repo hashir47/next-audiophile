c9243c6df55144fe5190cb8288874a8df2b0fd41
hashir tariq <hashir.tariq@evampsaanga.com>
2023-12-07 14:32:05 +0500

    replaced ' with &apos;

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,9 +41,9 @@ const Footer = () => {
           <li className="w-full lg:w-2/4">
             <p className="text-center md:text-start">
               Audiophile is an all in one stop to fulfill your audio needs.
-              We're a small team of music lovers and sound specialists who are
-              devoted to helping you get the most out of personal audio. Come
-              and visit our demo facility - we’re open 7 days a week.
+              We&apos;re a small team of music lovers and sound specialists who
+              are devoted to helping you get the most out of personal audio.
+              Come and visit our demo facility - we&apos;re open 7 days a week.
             </p>
           </li>
         </ul>
